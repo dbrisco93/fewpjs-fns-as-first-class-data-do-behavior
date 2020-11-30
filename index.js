7d0f1cd8557510428f3eb53fbc7b8b9c33a2b6a8
dbrisco93 <dbrisco93@gmail.com>
2020-11-30 16:52:47 -0600

    commit tree 446b1a781fa3c87ce7a815d172c916c1899ff99c

--- index.js
+++ index.js
@@ -10,4 +10,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString) {
+  const convertTime = timeString.split(':').join("")
+  const intTime = parseInt(convertTime)
+
+  if (intTime < 1159){ 
+    return "Good Morning"
+  }
+  if (intTime >= 1200 && intTime < 1700){
+    return "Good Afternoon"}
+  if (intTime >= 1700){
+    return "Good Evening"}
+
+
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(greet) {
+ const response = document.getElementById('greeting')
+ response.innerText = greet
+}
